cb0b3ea3d8988c7b2ac201e03ba6b299c8d18e7e
ak471314 <519789250@qq.com>
2024-04-08 13:36:13 +0800

    fix

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -56,7 +56,7 @@ def push_parse(bar,clash,countrify,info):
 
 
 def push(list, outfile):
-    print("push clash len:" + len(list))
+    print("push clash len:" + str(len(list)))
     global count, country_count
     count = 1
     country_count = {}
