d5bd83b8a34013722e7d101e28851897ebcab7f5
ak471314 <519789250@qq.com>
2024-04-08 12:54:03 +0800

    Update config.js

--- utils/rm/config.js
+++ utils/rm/config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: ak471314 519789250@qq.com
+ * @Date: 2024-04-07 16:24:50
+ * @LastEditors: ak471314 519789250@qq.com
+ * @LastEditTime: 2024-04-08 12:52:17
+ * @FilePath: \NiceVPN\utils\rm\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports={
     nodeAddName:' → free',
     dnsServers:['223.5.5.5','114.114.114.114']
